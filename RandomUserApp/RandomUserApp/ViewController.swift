4c0fbbd97d3f6f75d63ef47dffea3b2e4dba2918
dzk34 <khaledus34@gmail.com>
2025-04-08 14:19:11 +0200

    Initial commit

---
+++ RandomUserApp/RandomUserApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomUserApp
+//
+//  Created by Khaled on 08/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
